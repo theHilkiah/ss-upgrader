11df656bd3c7402c3c54f7a9f7afb0377af24b4f
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-07-12 22:29:06 -0600

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,16 +1,8 @@
-<?php
+<?php  
 
-$autoloaders = [    
-    __DIR__ . "/../autoload.php",
-    __DIR__ . "/../vendor/autoload.php",
-    __DIR__ . "/../../vendor/autoload.php",
-    __DIR__ . "/../../../vendor/autoload.php",
-    __DIR__ . "/../../../../vendor/autoload.php",
-];
-// Root from which to refer to src/. assists with bundling into a phar.
-foreach ($autoloaders as $path) {
-    if (file_exists($path)) {
-        require_once($path);
-        break;
-    }
-}
+  if(($v1 = realpath(__DIR__."/../autoload.php"))) require_once $v1;
+  elseif(($v2 = realpath(__DIR__."/../vendor/autoload.php"))) require_once $v2;
+  elseif(($v3 = realpath(__DIR__."/../../vendor/autoload.php"))) require_once $v3;
+  elseif(($v4 = realpath(__DIR__."/../../../vendor/autoload.php"))) require_once $v4;
+  elseif(($v5 = realpath(__DIR__."/../../../../vendor/autoload.php"))) require_once $v5;
+  else throw new Exception("No autoloader is available", 1);
