52f546d7c16fa1ab2ed53c249be6e0a298e4c95a
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-07-12 23:37:02 -0600

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,8 +1,13 @@
 <?php  
 
-  if(($v1 = realpath(__DIR__."/../autoload.php"))) require_once $v1;
-  elseif(($v2 = realpath(__DIR__."/../vendor/autoload.php"))) require_once $v2;
-  elseif(($v3 = realpath(__DIR__."/../../vendor/autoload.php"))) require_once $v3;
-  elseif(($v4 = realpath(__DIR__."/../../../vendor/autoload.php"))) require_once $v4;
-  elseif(($v5 = realpath(__DIR__."/../../../../vendor/autoload.php"))) require_once $v5;
-  else throw new Exception("No autoloader is available", 1);
+  $autoloaders[] = realpath(__DIR__."/../autoload.php");
+  $autoloaders[] = realpath(__DIR__."/../vendor/autoload.php");
+  $autoloaders[] = realpath(__DIR__."/../../vendor/autoload.php");
+  $autoloaders[] = realpath(__DIR__."/../../../vendor/autoload.php");
+  $autoloaders[] = realpath(__DIR__."/../../../../vendor/autoload.php");
+
+  foreac($autoloader as $loader){
+    if($loader && file_exists($loader)){
+      require_once($loader); break;
+    }
+  }
